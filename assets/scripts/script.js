f52238a36eb5d2c5cecbfa473a23462d1972764e
kevinchewning <klcbusiness@hotmail.com>
2021-07-02 09:49:59 -0400

    Wrote boilerplate code and added TODO comments

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+//Element Selecting Variables
+
+//Global Variables
+var ingredients;
+var myIngredients;
+var recipes;
+
+//API Key(s)
+//TODO Get API key from "https://www.thecocktaildb.com/api.php"
+
+//TODO Get API key from "https://www.mediawiki.org/wiki/API:Main_page#API_documentation"
+
+//TODO Get API key from "https://developers.google.com/youtube/v3"
+
+//Event Listeners
+
+//Functions
+//TODO Write a function to fetch the ingredient list from CocktailDB
+
+//TODO Write a function to render ingredient list with removal buttons from form 
+     //(Should utilize local storage to save our ingredients on refresh)
+
+//TODO Write a function to fetch recipes from CocktailDB
+
+//TODO Write a function to fetch wiki's for populated recipes
+
+//TODO Write a function to fetch youtube links for populated recipes
+
+//TODO Write a function to render popular recipe cards on page load
+
+//TODO Write a function to render recipe cards upon search
