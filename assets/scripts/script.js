a21da1db657ec75324d851be23d66eb1623e0a4a
kevinchewning <klcbusiness@hotmail.com>
2021-07-06 21:22:16 -0400

    wrote dropdown ingredient functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -33,14 +33,66 @@ fetch("https://the-cocktail-db.p.rapidapi.com/list.php?i=list", {
                ingredients.push(data.drinks[i].strIngredient1);
                }
           console.log(ingredients);
+          dropdownIngr();
      });
 
+//TODO Write a function to add all ingredients to dropdown
+function dropdownIngr () {
+     for(i = 0; i < ingredients.length; i++) {
+          var ingredient = $('<div>');
+          ingredient.text(ingredients[i]);
+          ingredient.attr('id', ingredients[i]);
+          ingredient.addClass('ingredient');
+
+          $('#myDropdown').append(ingredient);
+     }
+}
+
+//TODO Write a function to render dropdown menu
+function dropdown() {
+     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+     var input = document.getElementById("myInput");
+     var filter = input.value.toUpperCase();
+     var a = $('.ingredient')
+     for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          a[i].style.display = "";
+          } else {
+          a[i].style.display = "none";
+          }
+     }
+}
+
+//TODO Write a function that adds ingredient from dropdown to myIngredients when clicked.
 
 //TODO Write a function to render ingredient list with removal buttons from form 
      //(Should utilize local storage to save our ingredients on refresh)
      //Removal buttons should have a class called ".remove" for the event listener
 
 //TODO Write a function to fetch recipes from CocktailDB
+/*
+function fecthRecipes () {
+     var requestURL = "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + //myIngredients need stringed together here
+
+     fetch(requestURL, {
+          "method": "GET",
+          "headers": {
+               "x-rapidapi-key": "35d6c6f44bmshd692617dc44705dp131153jsnc3bf1d0d4e09",
+               "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+          }
+     })
+     .then(function (response) {
+          return response.json();
+     })
+     .then(function (data) {
+          console.log(data);
+     })
+}
+*/
 
 //TODO Write a function to fetch wiki's for populated recipes (may need to go within render recipe cards function)
 
